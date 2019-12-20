0f16103b3d62e32b32f175ef59219864a7756b4a
unknown <cambuf@gmail.com>
2019-12-20 08:53:02 -0700

    project day 4 problems ..

--- practice.js
+++ practice.js
@@ -17,7 +17,9 @@ var arr = [10,20,30];
   Return the first item in the given array.
 */
 
-//Code Here
+function first(arr){
+  return arr[0]
+}
 
 
 
@@ -32,7 +34,9 @@ var arr = [40,50,60];
   Return the last item in the given array.
 */
 
-//Code Here
+function last(arr){
+  return arr[2]
+}
 
 
 
@@ -47,7 +51,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
   Loop through the given array and alert every item in the array.
 */
 
-//Code Here
+function looper(family){ 
+  for(let i = 0; i <= arr.length - 1; i++){
+  alert(family[i])
+}
+}
 
 
 
@@ -62,7 +70,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
   Loop backwards, starting at the end of the letters array, alerting every item in the array.
 */
 
-//Code Here
+function reversedLooper(letters){
+  for(let i = letters.length - 1; i >= 0; i -- ){
+    alert(letters[i])
+  }
+}
 
 
 
@@ -77,9 +89,18 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
   Return an array that contains the even numbers from the nums array.
 */
 
-//Code Here
+
+function evenFinder(nums){
+  let evens = []
+  for(let i = 0; i <= nums.length - 1; i++){
+  if(nums[i] % 2 === 0){
+  evens.push(nums[i])
+  }
+  }
+  return evens
 
 
+  }
 
 
 
